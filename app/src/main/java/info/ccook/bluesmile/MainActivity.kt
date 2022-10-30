e1b74fc0d552685792f56b4a0192cd16e419124f
Chris Cook <chriscook@Chriss-MBP.attlocal.net>
2022-10-30 11:38:56 -0400

    Initial commit

---
+++ app/src/main/java/info/ccook/bluesmile/MainActivity.kt
@@ -0,0 +1,44 @@
+package info.ccook.bluesmile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import info.ccook.bluesmile.ui.theme.BlueSmileTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlueSmileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BlueSmileTheme {
+        Greeting("Android")
+    }
+}
